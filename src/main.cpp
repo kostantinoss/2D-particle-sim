c265eeb69f9699289f568dda4405f8f4d156969a
Konstantinos Chondralis <khondralis@gmail.com>
2026-01-11 03:32:57 +0200

    Clean up commented-out code in main.cpp
    Removed commented-out code for particle spawning timer.

--- src/main.cpp
+++ src/main.cpp
@@ -14,7 +14,6 @@ int main()
 
     ParticleSim particleSim(200, window.getSize());
     sf::Clock clock;
-    // sf::Clock spawn_timer;  // Timer for spawning particles (commented out for circle mode)
 
     while (window.isOpen())
     {
@@ -37,12 +36,6 @@ int main()
 
         sf::Time dt = clock.restart();
         particleSim.update(dt, mousePos, forceStrength);
-        
-        // Spawn a particle every 100ms (commented out for circle mode)
-        // if (spawn_timer.getElapsedTime().asMilliseconds() >= 100) {
-        //     particleSim.spawn_particle();
-        //     spawn_timer.restart();
-        // }
 
         window.clear();
         window.draw(particleSim);
